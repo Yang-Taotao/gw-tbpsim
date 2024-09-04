0161ee22cf72f8a34b56e1ddbc09664642389db5
Yang Taotao <yangtaotao0524@gmail.com>
2024-09-04 14:32:14 +0100

    refactor: rename test folder

--- tests/test.py
+++ tests/test.py
@@ -4,9 +4,20 @@ Main test file.
 
 # Imports
 import unittest
+import os
+import jax
 import jax.numpy as jnp
 import src.gw_tbpsim as gw
 from src.gw_tbpsim.constant import THETA_ARRAY, F_SIG
+
+# JAX settings
+jax.config.update("jax_enable_x64", True)
+# Setting - Manual memory allocation -> set to true if OOM occurs
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "true"
+# Setting - Set up presistent cache
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+jax.config.update("jax_compilation_cache_dir", ".jaxcache")
 
 
 # Tester
