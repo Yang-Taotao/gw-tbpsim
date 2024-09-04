8c355ea097f42e11176ba6f3d08bc1046fbe70fb
Yang Taotao <yangtaotao0524@gmail.com>
2024-09-04 14:14:01 +0100

    feat: add mc, eta configs and remove mass configs

--- src/gw_tbpsim/config.py
+++ src/gw_tbpsim/config.py
@@ -4,10 +4,11 @@ Config file
 
 Configurations for:
     - F_BASE: Frequency settings for signal and reference.
-    - M_BASE: Component mass settings for mass entries, same for m1, m2.
-    - PARAM_BASE: Initial param 
+    - MC_BASE: Chirp mass settings.
+    - ETA_BASE: Symmetric mass ratio settings. 
+    - PARAM_BASE: Initial param with m1, m2
         - [m1, m2, chi1, chi2, dist_mpc, tc, phic, inclination].
-    - THETA_BASE: Initial param
+    - THETA_BASE: Initial param with mc, eta
         - [mc, eta, chi1, chi2, dist_mpc, tc, phic, inclination].
 """
 
@@ -15,8 +16,10 @@ Configurations for:
 # =========================================================================== #
 # Freq - min, max, step size
 F_BASE = 24.0, 512.0, 0.5
-# Mass - min, max, step size
-M_BASE = 1.0, 21.0, 100
+# Mass - Chirp mass - min, max, step size
+MC_BASE = 1.0, 21.0, 100
+# MASS - Symmetric mass ratio - min, max, step size
+ETA_BASE = 0.05, 0.25, 100
 # Param base - m1, m2, chi1, chi2, dist_mpc, tc, phic, inclination
 PARAM_BASE = [36.0, 29.0, 0.0, 0.0, 40.0, 0.0, 0.0, 0.0]
 # Theta base - mc, eta, chi1, chi2, dist_mpc, tc, phic, inclination
