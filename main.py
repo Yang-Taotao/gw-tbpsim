d990f44606e5b8386ac3854b92873b5d966cf943
Yang Taotao <yangtaotao0524@gmail.com>
2024-09-04 14:11:32 +0100

    refactor: updates sample run commands

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-'''
+"""
 Main script file for testing purposes.
-'''
+"""
+
 # Imports
-import src.gw_tbpsim
-from src.gw_tbpsim.constant import THETA_ARRAY
-# Import JAX settings
-import src.gw_tbpsim.config
-# Set JAX settings
-src.gw_tbpsim.config.jax_settings()
+import src.gw_tbpsim as gw
+from src.gw_tbpsim.constant import THETA_ARRAY, F_SIG
 
 # Test - wf gen - 1d
-hp_test, hc_test = src.gw_tbpsim.waveform(THETA_ARRAY)
-print(f"hp_test.shape: {hp_test.shape}")
-print(f"hc_test.shape: {hc_test.shape}")
-# Test - wf normalization - 1d
-hp_norm, hc_norm = src.gw_tbpsim.waveform_norm(THETA_ARRAY)
-print(f"hp_norm.shape: {hp_norm.shape}")
-print(f"hc_norm.shape: {hc_norm.shape}")
+waveform_hp = gw.waveform_hp(THETA_ARRAY, F_SIG)
+waveform_hc = gw.waveform_hc(THETA_ARRAY, F_SIG)
+grad_hp = gw.grad_hp(THETA_ARRAY)
+grad_hc = gw.grad_hc(THETA_ARRAY)
