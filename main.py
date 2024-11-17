5472d2473f1d0fdab145d38e94e45814e7ee0bf9
Yang Taotao <yangtaotao0524@gmail.com>
2024-11-17 20:29:36 +0000

    chore: main testing script update, add temp gradiant plotter for result checking.

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 Main script file for testing purposes.
 """
 
+# %%
 # Imports
 import os
 import jax
+import matplotlib.pyplot as plt
 import src.gw_tbpsim as gw
 from src.gw_tbpsim.constant import THETA_ARRAY, F_SIG
 
+
+# %%
 # JAX settings
 jax.config.update("jax_enable_x64", True)
 # Setting - Manual memory allocation -> set to true if OOM occurs
@@ -17,8 +21,24 @@ jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
 jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
 jax.config.update("jax_compilation_cache_dir", ".jaxcache")
 
-# Test - wf gen - 1d
-waveform_hp = gw.waveform_hp(THETA_ARRAY, F_SIG)
-waveform_hc = gw.waveform_hc(THETA_ARRAY, F_SIG)
+# %%
+# Test - grad calc
 grad_hp = gw.grad_hp(THETA_ARRAY)
 grad_hc = gw.grad_hc(THETA_ARRAY)
+
+
+# %%
+# Test - grad plot
+def grad_plot(f_sig, d_hp, d_hc):
+    """Simple gradiant plot display"""
+    fig, ax = plt.subplots()
+    ax.plot(f_sig, d_hp.real[:, 0], label="d hp.real")
+    ax.plot(f_sig, d_hc.imag[:, 0], label="d hc.imag")
+    ax.set(xlabel="sig", ylabel="d_h", xscale="log")
+    ax.legend()
+    fig.tight_layout()
+    plt.show()
+    plt.close()
+
+
+grad_plot(F_SIG, grad_hp, grad_hc)
