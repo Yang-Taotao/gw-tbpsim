22523a1e590b91eebc7702f03cc1c07a60001851
Yang Taotao <yangtaotao0524@gmail.com>
2024-11-24 19:29:34 +0000

    chore: adjust main script calls. Simple density statistics working.

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ Main script file for testing purposes.
 # Imports
 import os
 import jax
-import matplotlib.pyplot as plt
 import src.gw_tbpsim as gw
-from src.gw_tbpsim.constant import THETA_ARRAY, F_SIG
+from src.gw_tbpsim.constant import THETA_ARRAY
 
 
 # %%
@@ -22,23 +21,7 @@ jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
 jax.config.update("jax_compilation_cache_dir", ".jaxcache")
 
 # %%
-# Test - grad calc
-grad_hp = gw.grad_hp(THETA_ARRAY)
-grad_hc = gw.grad_hc(THETA_ARRAY)
-
-
-# %%
-# Test - grad plot
-def grad_plot(f_sig, d_hp, d_hc):
-    """Simple gradiant plot display"""
-    fig, ax = plt.subplots()
-    ax.plot(f_sig, d_hp.real[:, 0], label="d hp.real")
-    ax.plot(f_sig, d_hc.imag[:, 0], label="d hc.imag")
-    ax.set(xlabel="sig", ylabel="d_h", xscale="log")
-    ax.legend()
-    fig.tight_layout()
-    plt.show()
-    plt.close()
-
-
-grad_plot(F_SIG, grad_hp, grad_hc)
+# Test - det
+det_hp = gw.log_sqrt_det_hp(THETA_ARRAY)
+det_hc = gw.log_sqrt_det_hc(THETA_ARRAY)
+print(det_hp, det_hc)
