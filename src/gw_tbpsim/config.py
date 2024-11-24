2dc8103d4e52fa49a8c466fe72c7ce6c4a4b21b8
Yang Taotao <yangtaotao0524@gmail.com>
2024-11-24 12:50:38 +0000

    fix: set to correct dist_mpc value.

--- src/gw_tbpsim/config.py
+++ src/gw_tbpsim/config.py
@@ -15,13 +15,13 @@ Configurations for:
 # Initial CONST config
 # =========================================================================== #
 # Freq - min, max, step size
-F_BASE = 20.0, 500.0, 0.5
+F_BASE = 20.0, 520.0, 0.5
 # Mass - Chirp mass - min, max, step size
 MC_BASE = 1.0, 21.0, 100
 # MASS - Symmetric mass ratio - min, max, step size
 ETA_BASE = 0.05, 0.25, 100
 # Param base - m1, m2, chi1, chi2, dist_mpc, tc, phic, inclination
-PARAM_BASE = [36.0, 29.0, 0.0, 0.0, 40.0, 0.0, 0.0, 0.0]
+PARAM_BASE = [36.0, 29.0, 0.0, 0.0, 400.0, 0.0, 0.0, 0.0]
 # Theta base - mc, eta, chi1, chi2, dist_mpc, tc, phic, inclination
-THETA_BASE = [28.0956, 0.2471, 0.0, 0.0, 40.0, 0.0, 0.0, 0.0]
+THETA_BASE = [28.0956, 0.2471, 0.0, 0.0, 400.0, 0.0, 0.0, 0.0]
 # =========================================================================== #
